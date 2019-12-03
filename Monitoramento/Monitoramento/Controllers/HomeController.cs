9e91c7534f9f8314f719cded9855f534ba8a04d7
Felipe Freitas <felipe_freittas@outlook.com>
2019-12-02 22:59:46 -0300

    Monitoramento

--- Monitoramento/Monitoramento/Controllers/HomeController.cs
+++ Monitoramento/Monitoramento/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Monitoramento.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,66 @@ namespace Monitoramento.Controllers
 {
     public class HomeController : Controller
     {
+        private Conta _conta;
+
+        public HomeController()
+        {
+            try
+            {
+                _conta = (Conta)System.Web.HttpContext.Current.Session["conta"];
+            }
+            catch
+            {
+                _conta = new Conta();
+            }
+
+            if (_conta == null)
+                _conta = new Conta();
+
+            System.Web.HttpContext.Current.Session["conta"] = _conta;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SendNumber(int? valor)
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            if (valor != null)
+            {
+
+                if (valor.Value > 100)
+                {
+                    var contaFinalizada = (Conta)System.Web.HttpContext.Current.Session["conta"];
+
+                    _conta = null;
+
+                    System.Web.HttpContext.Current.Session["conta"] = _conta;
+
+                    ViewBag.Titulo = "Operação Finalizada";
+
+                    return View("Monitor", contaFinalizada);
+                }
+
+                if (valor.Value % 2 == 0)
+                    _conta.ListaNumPar.Add(valor.Value);
+                else
+                    _conta.ListaNumImpar.Add(valor.Value);
+
+                System.Web.HttpContext.Current.Session["conta"] = _conta;
+
+            }
+
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Contact()
+        public ActionResult Monitor()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            ViewBag.Titulo = "Monitor";
+            return View(_conta);
         }
+
     }
 }
---
+++ Monitoramento/Monitoramento/Models/conta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Monitoramento.Models
+{
+    public class Conta
+    {
+        public List<int> ListaNumPar { get; set; }
+        public List<int> ListaNumImpar { get; set; }
+        public List<string> Extrato { get; set; }
+        public decimal ContaCorrente { get; set; }
+
+        public Conta()
+        {
+            ListaNumPar = new List<int>();
+            ListaNumImpar = new List<int>();
+            Extrato = new List<string>();
+            ContaCorrente = 200;
+        }
+    }
+}
